1b411fb3720d544f2b5c2bdfb4cf5f985d4b35b5
phantran <phantran197@gmail.com>
2021-04-05 15:31:27 +0200

    Updates

---
+++ precache-manifest.373222e37d5b3929b126fbf9bd62a2f8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "727a8d423678411a7cecba1427530035",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "e3ab6063b220a4feb97f",
+    "url": "/web/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "9af24398cefb323417dc",
+    "url": "/web/static/css/main.179f326f.chunk.css"
+  },
+  {
+    "revision": "e3ab6063b220a4feb97f",
+    "url": "/web/static/js/2.53447354.chunk.js"
+  },
+  {
+    "revision": "9af24398cefb323417dc",
+    "url": "/web/static/js/main.bb37a209.chunk.js"
+  },
+  {
+    "revision": "0d49b18e61df0278a54d",
+    "url": "/web/static/js/runtime-main.c915405d.js"
+  },
+  {
+    "revision": "01806726735cfe1e5a78f3f286c3f7d2",
+    "url": "/web/static/media/avatar.01806726.png"
+  },
+  {
+    "revision": "c893c9ad8f5500ef7bb1abc2e85a2dbe",
+    "url": "/web/static/media/common-banner.c893c9ad.png"
+  },
+  {
+    "revision": "c6e4e3a0c22662111e37a92adf4146c5",
+    "url": "/web/static/media/header-img.c6e4e3a0.png"
+  },
+  {
+    "revision": "338182ea9b1618f20469f72be3e24faf",
+    "url": "/web/static/media/home-banner-bg.338182ea.png"
+  },
+  {
+    "revision": "bfb9ef3477e88b1e97f3312130dad23a",
+    "url": "/web/static/media/pagenotfound.bfb9ef34.jpg"
+  },
+  {
+    "revision": "da7e9475e9b313c901138681ca5dfce0",
+    "url": "/web/static/media/working_dev.da7e9475.jpg"
+  }
+]);
